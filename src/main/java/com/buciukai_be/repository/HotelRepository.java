4ce99aebdf3059d5bd1fa21446bc46fedb337366
Drunkeye <stonciusjustas@gmail.com>
2025-12-22 09:19:56 +0200

    Search feature backend
    Paieška grąžina reikiamus laukus filtravimui ir rikiavimui

--- src/main/java/com/buciukai_be/repository/HotelRepository.java
+++ src/main/java/com/buciukai_be/repository/HotelRepository.java
@@ -8,6 +8,9 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+import java.math.BigDecimal;
+import org.apache.ibatis.annotations.Param;
+
 
 import com.buciukai_be.model.Hotel;
 
@@ -75,5 +78,78 @@ public interface HotelRepository {
     FROM buciukai.hotel
 """)
 List<Hotel> findAll();
+
+@Select({
+        "<script>",
+        "SELECT",
+        "  h.id,",
+        "  h.name,",
+        "  h.address,",
+        "  h.city,",
+        "  h.country,",
+        "  h.postal_code  AS postalCode,",
+        "  h.phone_number AS phoneNumber,",
+        "  h.email,",
+        "  h.star_rating  AS starRating,",
+        "  h.description,",
+        "  h.total_rooms  AS totalRooms,",
+        "  MIN(r.price)   AS lowestPrice",
+        "FROM buciukai.hotel h",
+        "JOIN buciukai.room r ON r.hotel_id = h.id",
+        "WHERE 1=1",
+        "  <if test=\"name != null and name != ''\">",
+        "    AND LOWER(h.name) LIKE CONCAT('%', LOWER(#{name}), '%')",
+        "  </if>",
+        "  <if test=\"city != null and city != ''\">",
+        "    AND LOWER(h.city) LIKE CONCAT('%', LOWER(#{city}), '%')",
+        "  </if>",
+        "  <if test='starRating != null'>",
+        "    AND h.star_rating = #{starRating}",
+        "  </if>",
+        "  <if test='priceFrom != null'>",
+        "    AND r.price &gt;= #{priceFrom}",
+        "  </if>",
+        "  <if test='priceTo != null'>",
+        "    AND r.price &lt;= #{priceTo}",
+        "  </if>",
+        "  <if test='roomTypeId != null'>",
+        "    AND r.room_type_id = #{roomTypeId}",
+        "  </if>",
+        "  <if test='bedTypeId != null'>",
+        "    AND r.bed_type_id = #{bedTypeId}",
+        "  </if>",
+        "  <choose>",
+        "    <when test='onlyAvailable == null or onlyAvailable == true'>",
+        "      AND r.is_available = TRUE",
+        "    </when>",
+        "    <otherwise>",
+        "      AND r.is_available = FALSE",
+        "    </otherwise>",
+        "  </choose>",
+        "GROUP BY",
+        "  h.id, h.name, h.address, h.city, h.country, h.postal_code, h.phone_number, h.email, h.star_rating, h.description, h.total_rooms",
+        "<choose>",
+        "  <when test=\"sortBy == 'price'\">",
+        "    ORDER BY lowestPrice ASC, h.name ASC",
+        "  </when>",
+        "  <otherwise>",
+        "    ORDER BY h.name ASC",
+        "  </otherwise>",
+        "</choose>",
+        "</script>"
+})
+List<Hotel> searchHotels(
+        @Param("name") String name,
+        @Param("city") String city,
+        @Param("starRating") Integer starRating,
+        @Param("priceFrom") BigDecimal priceFrom,
+        @Param("priceTo") BigDecimal priceTo,
+        @Param("roomTypeId") Integer roomTypeId,
+        @Param("bedTypeId") Integer bedTypeId,
+        @Param("onlyAvailable") Boolean onlyAvailable,
+        @Param("sortBy") String sortBy
+
+);
+
 
 }
